3853fa0eab5bced01c18a25f39250db414acbff1
Alexander Roikier Møller <alex6308@elev.tec.dk>
2018-08-15 13:23:48 +0200

    <Færdigt resultat, der kan regnes med binære, hexadecimale værdier>

---
+++ Opgave4_side16/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opgave4_side16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input "decimaltal"
+            Console.WriteLine("Indtast decimatal");
+            int decimaltal = Convert.ToInt32(Console.ReadLine());
+
+            //Der omregnes til binære tal
+            string binary = Convert.ToString(decimaltal, 2);
+            string hex = decimaltal.ToString("X");
+
+            //Output
+            Console.Clear();
+            Console.WriteLine("{0} Decimal is {1} in binary",decimaltal ,binary);
+            Console.WriteLine("{0} Decimal is {1} in hexadecimal", decimaltal, hex);
+            //Slut
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
